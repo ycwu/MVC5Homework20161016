fcac89c600b5773c328ad2c3263ec0a02138f72a
ycwu <ycwu@NB-00021>
2016-10-16 21:37:17 +0800

    加入專案檔案。

---
+++ MVC5Homework1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5Homework1.Startup))]
+namespace MVC5Homework1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
